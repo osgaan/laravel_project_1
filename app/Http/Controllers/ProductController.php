d24f0a125be7ae5dcf4c7e4f2378248304dd454d
OSGAAN <osgaan@gmail.com>
2020-04-08 13:39:28 -0500

    where ids entre rango

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -17,10 +17,7 @@ class ProductController extends Controller
     {
       // $products = Product::latest()->Paginate(5);
 
-       $products = Product::where('name', 'NARANJA')->Paginate(5);
-
-
-
+       $products = Product::where('id', '>','30')->Paginate(5);
 
 //dd($products);
 
